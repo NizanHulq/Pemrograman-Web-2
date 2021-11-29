fee7994644c10118162ef65b0da3e901f683b0b2
NizanHulq <nizandiaulhaq@gmail.com>
2021-11-29 14:51:44 +0700

    tambah seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,18 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Users::create([
-            'level' => 'user',
-            'name' => 'Rama Kurus',
-            'email' => 'ramakurus@gmail.com',
-            'password' => bcrypt('ramakurus')
-            ]);
-    
-        Users::create([
+        \App\User::create([
             'level' => 'admin',
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('admin')
-            ]);
+        ]);
+        
     }
 }
